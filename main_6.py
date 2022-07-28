d98321653d06c8ea0c0a9a1de301973727b215f7
vera <lerika250293@mail.ru>
2022-07-28 16:21:34 +0300

    add'

--- main_6.py
+++ main_6.py
@@ -1,79 +1,9 @@
-class Hotels:
-    status = {0: 'свободно', 1: 'на ремонте', 2: 'занято'}
-
-    def __init__(self, count_rooms):
-        self.count_rooms = count_rooms
-        self.all_rooms = {}
-        self.status_free_room = []
-        self.status = 'свободно'
-
-    def get_count_rooms(self):
-        return self.count_rooms
-
-
-h = Hotels(3)
-
-
 class Rooms:
-    ALL_ROOMS = {1: 'свободно', 2: 'свободно', 3: 'свободно'}
+    ALL_ROOMS = {1: 'свободно', 2: 'занято', 3: 'на ремонте'}
 
     def __init__(self, number):
         self.number = number
-
         self.status = 'свободно'
-        self.choose_room = 0
-        self.free_rooms = self.status_free()
-
-    def get_rooms(self):
-        return self.print_numbers()
-
-    def status_free(self):
-        self.status_free_room = []
-        for k, v in self.ALL_ROOMS.items():
-            if v == 'свободно':
-                self.status_free_room.append(k)
-        return self.status_free_room
-
-    def change_status_occupied(self, index):
-        self.ALL_ROOMS[index] = 'занято'
-        return self.ALL_ROOMS
-
-    def take_room(self):
-        self.status_free_rooms = self.status_free()
-        self.choose_room = self.status_free_rooms.pop()
-        self.change_status_occupied(self.choose_room)
-        return self.choose_room
-
-    def take_room_choose(self, number):
-        self.number = number
-        self.status_free_rooms = self.status_free()
-        self.status_free_rooms.remove(self.number)
-        self.change_status_occupied(self.number)
-        return self.status_free_rooms
-
-    def change_status_free(self, index):
-        self.ALL_ROOMS[index] = 'свободно'
-        return self.ALL_ROOMS
-
-    def change_status_repear(self, index):
-        self.ALL_ROOMS[index] = 'на ремонте'
-        return self.ALL_ROOMS
-
-    def print_numbers(self):
-        return self.ALL_ROOMS
-
-    def print_free_numbers(self, number):
-        self.number = number
-        return self.number
-
-    def cheak_repair(self, number):
-        self.room_number_repair = int(number)
-        print(self.ALL_ROOMS[self.room_number_repair])
-        if self.ALL_ROOMS[self.room_number_repair] == 'на ремонте':
-            print(self.ALL_ROOMS[self.room_number_repair])
-            return self.room_number_repair
-        else:
-            return False
 
 
 class Human:
@@ -111,87 +41,100 @@ class Human:
 class Hotel:
 
     def __init__(self, count_room):
-        self.rooms = [Rooms(i) for i in range(1, count_room+1)]
+        self.rooms = [Rooms(i) for i in range(1, count_room + 1)]
 
-        self.room = Rooms()
-
-    def change_status(self, status, number_room):
+    def change_status_repair(self, number_room):
+        self.number = number_room
         for room in self.rooms:
-            if room.number == number_room:
-                room.status = status
-
-    def get_status_room_all(self):
-        for room in self.rooms:
-            print(f"Номер: {room.number} - {room.status}")
-
-    def get_state(self):
-        return self.room.get_rooms()
-
-    def get_free_rooms(self):
-        return self.room.status_free()
+            if room.number == self.number:
+                print(room.number)
+                if room.status == 'свободно' and room.status != 'занято':
+                    room.status = 'на ремонте'
+                    return True
+                else:
+                    return False
 
     def register_person(self, name):
         self.name = Human(name)
         return self.name.name
 
-    def take_non_choose_room(self):
-        self_choose_room = self.room.take_room()
-        self.room.get_rooms()
-        self.name.get_number(self_choose_room)
-        self.room.change_status_occupied(self_choose_room)
-        return self.name.get_info()
+    def get_status_room_all(self):
+        for room in self.rooms:
+            print(f"Номер: {room.number} - {room.status}")
 
-    def take_choose_room(self, number):
-        self.number = number
-        self.free = self.room.status_free()
-        if self.number in self.free:
-            self.choose_room = self.room.take_room_choose(self.number)
-            self.name.get_number(self.number)
-            return Human.get_info()
-        else:
-            return False
+    def take_choose_room(self, cur_number):
+        self.cur_number = cur_number
+        for room in self.rooms:
+            if room.number == self.cur_number:
+                if room.status == 'свободно':
+                    room.status = 'занято'
+                    self.name.get_number(self.cur_number)
+                    return Human.get_info()
+                else:
+                    return False
+
+    def free_room_after_occupied(self, cur_number):
+        self.cur_number = cur_number
+        for room in self.rooms:
+            if room.number == int(self.cur_number):
+                if room.status != 'свободно':
+                    room.status = 'свободно'
+                    return True
+                return False
+
+    def free_room(self, cur_number):
+        self.cur_number = cur_number
+        for room in self.rooms:
+            if room.number == self.cur_number:
+                print(room.number)
+                if room.status != 'свободно' and room.status != 'занято':
+                    room.status = 'свободно'
+                    print(room.status)
+                    return True
+                return False
 
     def cheack_out(self, room):
         self.number = room
         self.room_number = self.name.cheak_leaving(self.number)
         if self.room_number:
-            self.name.leaving(self.room_number)
-            return self.room.change_status_free(self.room_number)
+            print(self.room_number)
+            print(self.name.leaving(self.room_number))
+            print(self.free_room_after_occupied(self.number))
+            return 'ok'
         else:
             return False
 
-    def status_for_repair(self, number):
-        self.number_repair = number
-        self.free = self.room.status_free()
-        if self.number_repair in self.free:
-            return self.room.change_status_repear(self.number_repair)
-        else:
-            return False
+    def take_non_choose_room(self):
+        self.free_rooms = []
+        for room in self.rooms:
+            if room.status == 'свободно':
+                self.free_rooms.append(room)
+        self.free_rooms[0].status = 'занято'
+        self.name.get_number(self.free_rooms[0].number)
+        return self.name.get_info()
 
-    def status_for_free(self, number):
-        self.number_free = number
-        self.cheeak_number_free = self.room.cheak_repair(self.number_free)
-        if self.cheeak_number_free:
-            self.room.change_status_free(self.number_free)
-            return self.room.get_rooms()
-        else:
-            return False
+    def get_free_rooms(self):
+        self.free_rooms = []
+        for room in self.rooms:
+            if room.status == 'свободно':
+                self.free_rooms.append(room.number)
+        return self.free_rooms
 
 
-numbers = input('Сколько комнат в ателе? ')
+numbers = int(input('Сколько комнат в отеле? '))
 h = Hotel(numbers)
 
 while True:
     action = input(
-        '''Выберите действие: 
-        1 посмотреть номерной фонд 
-        2 посмотреть свободные номера, 
-        3 заселить/выселить 
-        4 сменить статус номера 
+        '''Выберите действие:
+        1 посмотреть номерной фонд
+        2 посмотреть свободные номера,
+        3 заселить/выселить
+        4 сменить статус номера
         5 список постояльцев''')
 
     if action == "1":
-        print(h.get_state())
+        print(h.get_status_room_all())
     elif action == '2':
         print(h.get_free_rooms())
     elif action == '3':
@@ -216,6 +159,7 @@ while True:
             h.register_person(name)
             try:
                 if h.cheack_out(room):
+                    print(h.cheack_out(room))
                     print(Human.get_info())
                 else:
                     print(f'Проверьте соотвествие комнаты и постояльца')
@@ -225,13 +169,13 @@ while True:
         action_5 = int(input(f"Введите номер комнаты: "))
         action_7 = int(input(f"Установить статус 1 свободен 2 на ремонте: "))
         if action_7 == 2:
-            if h.status_for_repair(action_5):
-                print(h.get_state())
+            if h.change_status_repair(action_5):
+                print(h.get_status_room_all())
             else:
                 print(f'{action_5} не может встать на ремонт он занят')
         elif action_7 == 1:
-            if h.status_for_free(action_5):
-                print(h.get_state())
+            if h.free_room(action_5):
+                print(h.get_status_room_all())
             else:
                 print(f'{action_5} не может встать встать на свободен он не был на ремонте')
 
